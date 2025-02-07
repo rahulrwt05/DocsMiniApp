bfd025d40a4e8d6124a1191e4f2a175ed2ef9fc2
Rahul Singh Rawat <125528268+rahulrwt05@users.noreply.github.com>
2025-02-07 13:08:45 +0530

    Update Foreground.jsx

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,40 +1,38 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
+
 import Card from "./Card";
 
 function Foreground() {
   const ref = useRef(null);
-
   const data = [
     {
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      filesize: ".9mb",
+      desc: "Manage your files with ease using our intuitive interface.",
+      filesize: "1.2MB",
       close: true,
-      tag: { isOpen: true, tagTitle: "Download Now..", tagColor: "green" },
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
     },
     {
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      filesize: ".9mb",
+      desc: "Securely upload and share documents in just a few clicks.",
+      filesize: "750KB",
       close: true,
-      tag: { isOpen: true, tagTitle: "Uploading..", tagColor: "blue" },
+      tag: { isOpen: true, tagTitle: "Uploading", tagColor: "blue" },
     },
     {
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      filesize: ".9mb",
+      desc: "Access your stored files anytime, anywhere with cloud sync.",
+      filesize: "2.5MB",
       close: true,
-      tag: { isOpen: false, tagTitle: "Download No..", tagColor: "green" },
+      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green" },
     },
   ];
   return (
-    <>
-      <div
-        ref={ref}
-        className=" fixed top-0 left-0 w-full z-[3] h-full flex gap-10 flex-wrap p-5"
-      >
-        {data.map((item, index) => (
-          <Card data={item} reference={ref} />
-        ))}
-      </div>
-    </>
+    <div
+      ref={ref}
+      className=" fixed top-0 left-0 w-full z-[3] h-full flex gap-10 flex-wrap p-5"
+    >
+      {data.map((item, index) => (
+        <Card data={item} reference={ref} />
+      ))}
+    </div>
   );
 }
 
